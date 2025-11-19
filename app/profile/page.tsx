693b19dc1115aba2324d700bd36ff74a0744056e
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2025-11-20 00:48:18 +0530

    update profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,17 @@
+import { Suspense } from "react";
 import ProfileContent from "./Components/ProfileContent"
+import { Box, CircularProgress } from "@mui/material";
 
 const ProfilePage = () => {
-  return <ProfileContent />
+  return (
+    <Suspense fallback={
+      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+        <CircularProgress />
+      </Box>
+    }>
+      <ProfileContent />
+    </Suspense>
+  );
 }
 
 export default ProfilePage
